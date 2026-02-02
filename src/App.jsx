b24146726e039bb107d3ef775367e14d0df450f4
suri__ <suri4498@hs.ac.kr>
2026-02-02 22:22:40 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,24 @@ import Community from "./pages/Community";
 import CommunityWrite from "./pages/CommunityWrite";
 import CommunityDetail from "./pages/CommunityDetail";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";      // 2026.02.02 추가
-import MyPage from "./pages/MyPage";
+import Signup from "./pages/Signup";
+import MyPageLayout from "./pages/MyPage/MyPageLayout";
+import MyPageHome from "./pages/MyPage/MyPageHome";
+import MyInfoEdit from "./pages/MyPage/MyInfoEdit";
+import LikeList from "./pages/MyPage/LikeList";
+import CalendarPage from "./pages/MyPage/CalendarPage";
+import PartyHistory from "./pages/MyPage/PartyHistory";
+import Withdraw from "./pages/MyPage/Withdraw";
+import MyReviews from "./pages/MyPage/MyReviews";
+import ReviewWrite from "./pages/MyPage/ReviewWrite";
+import ReviewEdit from "./pages/MyPage/ReviewEdit";
+import MyPartyPosts from "./pages/MyPage/MyPartyPosts";
+
+
 import Benefits from "./pages/Benefits";
 import PartyDetail from "./pages/PartyDetail";
+
+import RequireAuth from "./auth/RequireAuth"; //2026.02.02 추가
 
 export default function App() {
   return (
@@ -33,8 +47,30 @@ export default function App() {
         <Route path="/benefits" element={<Benefits />} />
 
         <Route path="/login" element={<Login />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/signup" element={<Signup />} />  {/* 2026.02.02 추가 */}
+
+        {/* 2026.02.02 */}
+        <Route
+          path="/mypage"
+          element={
+            <RequireAuth>
+              <MyPageLayout />
+            </RequireAuth>
+          }
+        >
+          <Route index element={<MyPageHome />} />
+          <Route path="info" element={<MyInfoEdit />} />
+          <Route path="likes" element={<LikeList />} />
+          <Route path="calendar" element={<CalendarPage />} />
+          <Route path="parties" element={<PartyHistory />} />
+          <Route path="withdraw" element={<Withdraw />} />
+          <Route path="reviews" element={<MyReviews />} />
+          <Route path="reviews/new" element={<ReviewWrite />} />
+          <Route path="reviews/:id/edit" element={<ReviewEdit />} />
+          <Route path="posts" element={<MyPartyPosts />} />
+
+        </Route>
+
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Layout>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { AuthProvider } from "./auth/AuthContext";
 import "./index.css";
+
+console.log("main.jsx loaded", AuthProvider); //  2026.02.02
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
